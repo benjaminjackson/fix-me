3b4d4da8576b3531e625ce5fbda47c1cfbe0c777
Chamara Paul <chamara.paul@gmail.com>
2015-02-13 12:08:46 -0500

    a few fixes for fix-me

--- FixMe/MasterViewController.m
+++ FixMe/MasterViewController.m
@@ -11,14 +11,27 @@
 
 @interface MasterViewController ()
 
-@property NSArray *objects;
+@property (nonatomic, strong) NSArray *urls;
+@property (nonatomic, strong) NSArray *images;
+
+- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
+
 @end
 
 @implementation MasterViewController
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    self.objects = @[@{@"URL" : @"http://nytimes.com"}, @{@"URL" : @"http://wsj.com"}, @{@"URL" : @"http://ft.com"}, @{@"URL" : @"http://grantland.com"}, @{@"URL" : @"http://vicenews.com"}];
+    self.urls = @[[NSURL URLWithString:@"http://nytimes.com"],
+                  [NSURL URLWithString:@"http://wsj.com"],
+                  [NSURL URLWithString:@"http://ft.com"],
+                  [NSURL URLWithString:@"http://grantland.com"],
+                  [NSURL URLWithString:@"http://vicenews.com"]];
+    self.images = @[[self imageWithImage:[UIImage imageNamed:@"1.jpg"] scaledToSize:CGSizeMake(100, 50)],
+                    [self imageWithImage:[UIImage imageNamed:@"2.jpg"] scaledToSize:CGSizeMake(100, 50)],
+                    [self imageWithImage:[UIImage imageNamed:@"3.jpg"] scaledToSize:CGSizeMake(100, 50)],
+                    [self imageWithImage:[UIImage imageNamed:@"4.jpg"] scaledToSize:CGSizeMake(100, 50)],
+                    [self imageWithImage:[UIImage imageNamed:@"5.jpg"]scaledToSize:CGSizeMake(100, 50)]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,8 +44,8 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row % [self.objects count]];
-        [[segue destinationViewController] setDetailItem:object];
+        NSURL *url = self.urls[indexPath.row % [self.urls count]];
+        [[segue destinationViewController] setDetailItem:url];
     }
 }
 
@@ -48,13 +61,32 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     imageView.clipsToBounds = YES;
-    imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%ld.jpg", ((long)indexPath.row % 5 + 1)]];
+    imageView.image = [self.images objectAtIndex:((long)indexPath.row % 5)];
     cell.textLabel.text = [NSString stringWithFormat:@"Image %ld", (long)indexPath.row];
     cell.accessoryView = imageView;
     return cell;
+}
+
+#pragma mark - private
+
+//need to avoid super large images....
+//taken from the interwebs ---
+- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+    //UIGraphicsBeginImageContext(newSize);
+    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+    // Pass 1.0 to force exact pixel size.
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 @end
